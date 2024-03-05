d6045c41e6b733c3ccad10cc281b56d07db18864
AhmedAlzarka <ahmeh.alzarka@aucegypt.edu>
2024-03-05 12:58:39 +0200

    the LoginWindow

--- CinemaProject/loginwindow.cpp
+++ CinemaProject/loginwindow.cpp
@@ -1,4 +1,5 @@
 #include "loginwindow.h"
+#include "users.h"
 #include "ui_loginwindow.h"
 
 LoginWindow::LoginWindow(QWidget *parent)
@@ -6,9 +7,30 @@ LoginWindow::LoginWindow(QWidget *parent)
     , ui(new Ui::LoginWindow)
 {
     ui->setupUi(this);
+
+    ui->error->setVisible(false);
 }
 
 LoginWindow::~LoginWindow()
 {
     delete ui;
 }
+
+void LoginWindow::on_pushButton_3_clicked()
+{
+    QString username = ui->username->text();
+    QString password = ui->password->text();
+    bool flag = false;
+    for(int i = 0; i < 100; i++)
+    {
+        if(usernames[i] == username && password == passwords[i])
+        {
+            flag = true;
+            break;
+        }
+    }
+
+    if(!flag)
+        ui->error->setVisible(true);
+}
+
